5930b5eb63b94805b7dc5661302b217f190760d5
Daniel Ward <dward1502@gmail.com>
2019-12-10 10:38:20 -0800

    updated dependencies

--- routes/API.js
+++ routes/API.js
@@ -3,32 +3,77 @@ const router = express.Router();
 const PropInfo = require('../model/properties');
 
 router.get('/properties', (req,res,next) => {
-  PropInfo.find((err,properties) =>{
-    if(err) return next(err);
-    res.json(properties)
+  PropInfo.find().exec()
+  .then(docs => {
+    console.log(docs);
+    res.status(200).json(docs)
   })
-})
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json({
+      error:err
+    });
+  });
+});
 
 router.get('/properties/:address', (req,res,next) => {
   const query = req.params.address;
   console.log(query);
 
-  PropInfo.findOne({'Unit Street Address 1': query},(err,data) => {
-    if(err) return next(err);
-    res.json(data);
+  PropInfo.findOne({'Unit Street Address 1': query}).exec()
+  .then(docs => {
+    console.log(docs);
+    res.status(200).json(docs)
   })
-})
-
-router.get('/properties/bedrooms/:number', (req,res,next) => {
-  const query = req.params.number;
-  console.log(query);
-  PropInfo.find({'Bedrooms':{$eq: query}}, (err,data) => {
-    if(err) return next(err);
-    res.json(data);
-    console.log(data);
-  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error:err
+    });
+  });
 });
 
+// router.get('/properties/bedrooms/:number', (req,res,next) => {
+//   const query = req.params.number;
+//   console.log(query);
+
+//   PropInfo.find({'Bedrooms':{$eq: query}})
+//   .then(docs => {
+//     console.log(docs);
+//     res.status(200).json(docs)
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     res.status(500).json({
+//       error:err
+//     });
+//   });
+// });
+router.get('/properties/bedrooms/:number/available', (req,res,next) => {
+  const query = req.params.number;
+
+  console.log(query);
+  let address = [];
+
+  PropInfo.find({"Bedrooms":query, Available:"Yes"})
+  .then(docs => {
+
+    console.log(docs);
+    console.log(docs[0]["Unit Address"])
+    docs.map(data =>{
+      console.log(`Address in map ${data["Unit Address"]}`);
+      address.push(data["Unit Address"]);
+    })
+    res.json(docs)
+  })
+  .catch(err => {
+    console.log(err);
+    // res.status(500).json({
+    //   error:err
+    // });
+  });
+  console.log(address)
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -11,9 +11,24 @@ app.use(bodyParser.json());
 
 
 // mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://admin:Pointers619!@cluster0-ouidt.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true, dbName: "HomeTeamDB" }).then(() => console.log('connection successfull')).catch((err) => console.log(err));
+mongoose.connect(`mongodb+srv://daniel:pointers6191502@hometeam-ouidt.mongodb.net/admin?retryWrites=true&w=majority`,{ useNewUrlParser: true, useUnifiedTopology: true, dbName: "HomeTeamDB" }).then(() => console.log('connection successfull')).catch((err) => console.log(err));
 
 app.use('/api', properties);
+
+// app.use((req,res,next) => {
+//   const error = new Error('Not Found');
+//   error.status(404);
+//   next(error);
+// })
+
+// app.use((error,req,res,next) => {
+//   res.status(error.status || 500);
+//   res.json({
+//     error: {
+//       message: error.message
+//     }
+//   })
+// })
 
 
 app.listen(PORT, () => {
